ed9b0c6ef4e58d8418b61d5972728fb7d722ea5f
Mihail Mitov <mihail.mitov@droxic.com>
2023-03-05 13:46:35 +0200

    [server] model - user

---
+++ backend/models/userModel.js
@@ -0,0 +1,45 @@
+const mongoose = require("mongoose");
+
+const userSchema = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Please add a name"],
+    },
+    email: {
+      type: String,
+      required: [true, "Please add a email"],
+      unique: true,
+      trim: true,
+      match: [
+        /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+        "Please enter a valid Email",
+      ],
+    },
+    password: {
+      type: String,
+      required: [true, "Please add a password"],
+      minLength: [4, "Password must be min 4 length"],
+      maxLength: [24, "Password must not be more than 24 characters"],
+    },
+    photo: {
+      type: String,
+      required: [true, "Please add a photo"],
+      default: "https://i.ibb.co/4pDNDk1/avatar.png",
+    },
+    phone: {
+      type: String,
+      default: "+234",
+    },
+    bio: {
+      type: String,
+      default: "bio",
+      maxLength: [250, "bio must not be more than 250 characters"],
+    },
+  },
+  { timestamps: true }
+);
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
