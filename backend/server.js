697b5a71108b896e0785f2f495651958a1e29cb7
Mihail Mitov <mihail.mitov@droxic.com>
2023-03-05 13:46:20 +0200

    [server] middleware and init routes

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,16 @@ const cors = require("cors");
 
 const app = express();
 
+// MIDDLEWARES
+app.use(express.json())
+app.use(express.urlencoded({extended: false}))
+app.use(bodyParser.json())
+
+// ROUTES
+app.get('/', (req, res) => {
+  res.send('homepage')
+})
+
 const PORT = process.env.PORT || 5000;
 
 //connect to DB and start server
