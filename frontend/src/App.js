162ca5bc981ecee1f1b40925d7dbcbc3be5bf33e
Mihail Mitov <mihail.mitov@droxic.com>
2023-03-08 09:25:54 +0200

    [frontend] sidebar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,12 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Layout from "./components/layout/Layout";
+import Sidebar from "./components/sidebar/Sidebar";
 import Forgot from "./pages/Auth/Forgot";
 import Login from "./pages/Auth/Login";
 import Register from "./pages/Auth/Register";
 import Reset from "./pages/Auth/Reset";
+import Dashboard from "./pages/dashboard/Dashboard";
 import Home from "./pages/Home/Home";
-
 
 function App() {
   return (
@@ -15,6 +17,17 @@ function App() {
         <Route path="/register" element={<Register />} />
         <Route path="/forgot" element={<Forgot />} />
         <Route path="/resetpassword/:resetToken" element={<Reset />} />
+
+        <Route
+          path="/dashboard"
+          element={
+            <Sidebar>
+              <Layout>
+                <Dashboard />
+              </Layout>
+            </Sidebar>
+          }
+        />
       </Routes>
     </BrowserRouter>
   );
